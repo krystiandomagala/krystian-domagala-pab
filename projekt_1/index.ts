d3f001677b7212a06712dc4ec8356727d6eef3cd
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-03-23 13:07:35 +0100

    final

--- projekt_1/index.ts
+++ projekt_1/index.ts
@@ -4,3 +4,21 @@ app.get('/', function (req, res) {
   res.send('Hello World')  
 })  
 app.listen(3000) 
+
+app.get('/:operator/:num1/:num2', function (req, res) {
+  
+  let num1 = parseInt(req.params.num1);
+  let num2 = parseInt(req.params.num2);
+  let operator = req.params.operator;
+
+  if(operator == 'add')
+      res.send((num1+num2).toString());
+  else if(operator =='substract')
+      res.send((num1-num2).toString());
+  else if(operator =='multiply')
+      res.send((num1*num2).toString());
+  else if(operator =='divide')
+      res.send((num1/num2).toString());
+  else
+    res.send('Niepoprawna komenda!')
+});
